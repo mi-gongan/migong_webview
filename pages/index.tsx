f1d43fb70d2140d9747af54cf969c2fd558e7ee8
INBUM02 <kib0202@naver.com>
2022-11-14 01:30:42 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>migong</title>
+        <meta name="description" content="migong" />
+        <link rel="icon" href="/assets/migong.png" />
+      </Head>
+      <div>migong</div>
+    </div>
+  );
+}
